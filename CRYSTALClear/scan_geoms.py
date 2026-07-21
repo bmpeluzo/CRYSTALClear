c2173aa4c4f8033ce129e64c7bf8a938584eb530
Barbara M. T. C. Peluzo <bmpeluzo.smu@gmail.com>
2026-07-21 11:37:43 +0200

    It works for jobs that terminated correctly. Need to work on jobs that have error messages

--- CRYSTALClear/scan_geoms.py
+++ CRYSTALClear/scan_geoms.py
@@ -10,42 +10,54 @@ for i in reversed(scan_out.data):           # reversed loop because the scan inf
     if scan_info != -1:
         scan_0 = scan_out.data.index(i)
 
-        # finding the individual scans
+        # finding the individual scans and the line it finishes
         scan_dict = {}
         for j in scan_out.data[scan_0+1:]:
             scan_true = j.find('(')
+
             if scan_true != -1:
                 scan_line = scan_out.data.index(j)
                 scan_i = j.split('(')[0]
                 scan_dict.update({int(scan_i): scan_line})
+
+            # searches for the ending line
+            scan_term = j.find('SCANMODE')
+
+            if scan_term != -1:
+                scan_term_l = scan_out.data.index(j)
+                break
         break
 
 # Loop over the scans to build the individual plots:    
 for i in list(scan_dict):
-    if scan_out.terminated == False:
-    
-        if i != list(scan_dict)[-1]:         # check if we are in the last element
-            scan_points = pd.DataFrame(scan_out.data[scan_dict[i]+2:scan_dict[i+1]])
+
+    # check if we are in the last scan
+    if i == list(scan_dict)[-1]:
+        
+        # Two cases: scan terminated or not
+        if scan_out.terminated == True:
+            scan_points = pd.DataFrame(scan_out.data[scan_dict[list(scan_dict)[-1]]+2:scan_term_l])
         else:
             scan_points = pd.DataFrame(scan_out.data[scan_dict[list(scan_dict)[-1]]+2:len(scan_out.data)])
-        print(scan_points)
-        scan_points = scan_points.iloc[:,0].str.split('\s+', expand=True)
-        scan_points.iloc[:,1:3] = scan_points.iloc[:,1:3].astype('float')
-        print(scan_points)
+    else:
+        scan_points = pd.DataFrame(scan_out.data[scan_dict[i]+2:scan_dict[i+1]])
+        
+    scan_points = scan_points.iloc[:,0].str.split('\s+', expand=True)
+    scan_points.iloc[:,1:3] = scan_points.iloc[:,1:3].astype('float')
 
-        # Plot
-        plt.figure()
-        plt.xlabel('Displacement')
-        plt.ylabel('E - E$_{eq}$')
-        plt.title('Scan %d' %i)
+    # Plot
+    plt.figure()
+    plt.xlabel('Displacement')
+    plt.ylabel('E - E$_{eq}$')
+    plt.title('Scan %d' %i)
 
         
-        if scan_points.iloc[:,1].shape[0] % 2 == 0:      #check if we have an even number of points (in case of non terminated jobs)
-            mid_point = 0
-        else:
-            mid_point = scan_points[scan_points.iloc[:,1]==0].index[0]
-        plt.plot(scan_points.iloc[:,1], scan_points.iloc[:,2]-scan_points.iloc[mid_point,2], marker='o', linestyle='dashed')
-        plt.show()
+    if scan_points.iloc[:,1].shape[0] % 2 == 0:      #check if we have an even number of points (in case of non terminated jobs)
+        mid_point = 0
+    else:
+        mid_point = scan_points[scan_points.iloc[:,1]==0].index[0]
+    plt.plot(scan_points.iloc[:,1], scan_points.iloc[:,2]-scan_points.iloc[mid_point,2], marker='o', linestyle='dashed')
+    plt.show()
 
 
 
